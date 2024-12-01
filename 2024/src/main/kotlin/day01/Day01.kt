c41d8190c9d150666e0bcfa7eb0efc90c03ac314
Sohel Shaikh <sohelshaikh97@gmail.com>
2024-12-01 16:30:48 +0530

    Add Day01

--- 2024/src/main/kotlin/day01/Day01.kt
+++ 2024/src/main/kotlin/day01/Day01.kt
@@ -1,15 +1,37 @@
 package day01
 
 import org.example.Puzzle
+import kotlin.math.abs
 
+object Day01 : Puzzle {
+    private fun parseList(input: List<String>) = input.map { line ->
+        val (first, second) = line
+            .split(" ")
+            .filter { it.isNotEmpty() }
+            .map { it.trim().toLong() }
+        first to second
+    }
 
-object Day01: Puzzle {
     override fun part1(input: List<String>): Any {
-        return -1
+        val pairs = parseList(input)
+
+        val left = pairs.map { it.first }.sorted()
+        val right = pairs.map { it.second }.sorted()
+
+        return left.zip(right) { l, r -> abs(l - r) }.sum()
     }
 
     override fun part2(input: List<String>): Any {
-        return -1
+        val pairs = parseList(input)
+
+        val freqMap = pairs
+            .map { it.second }
+            .groupingBy { it }
+            .eachCount()
+
+        return pairs.sumOf { (first, _) ->
+            first * freqMap.getOrDefault(first, 0)
+        }
     }
 }
 
